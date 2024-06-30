29f16611224b492fc2ab60e6ff9c105bb0fba61e
Malikibdah <malikibdah88@gmail.com>
2024-07-01 00:49:23 +0300

    newadd

---
+++ 25.6.2024 new/25.06.2024 new/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._06._2024_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 solution
+
+            //string[] names = { "", "", "", "", "" };
+            //names[0] = Console.ReadLine();
+            //names[1] = Console.ReadLine();
+            //names[2] = Console.ReadLine();
+            //names[3] = Console.ReadLine();
+            //names[4] = Console.ReadLine();
+            //Array.Sort(names);
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+            //Console.WriteLine(names[4]);
+
+            // 2 solution;
+
+            //double x = 241;
+            //Console.WriteLine(x);
+
+            //char ch = 'M';
+            //Console.WriteLine(ch);
+
+            //string str = "Malik";
+            //Console.WriteLine(str);
+
+            //bool amStronge = true;
+            //Console.WriteLine(amStronge);
+
+            //int nu = 10;
+            //Console.WriteLine(nu);
+
+            //const int theold = 30;
+            //Console.WriteLine(theold);
+
+            // 3 solution
+
+            //string[] car = { "BMW", "AUDI", "MERCEDES" };
+            //Console.WriteLine(car[0] + "  " + car[1] + "  " + car[2]);
+            //Console.WriteLine("the lenth is : " + car.Length);
+
+            // 4 solution
+
+
+            //string firstname = Console.ReadLine();
+            //string lasttname = Console.ReadLine();
+            //string birthyear = Console.ReadLine();
+
+            //Console.WriteLine(firstname + " " + lasttname + " " + birthyear);
+
+            // 5 solution
+
+            //string[] num = new string[10];
+            //num[0] = Console.ReadLine();
+            //num[1] = Console.ReadLine();
+            //num[2] = Console.ReadLine();
+            //num[3] = Console.ReadLine();
+            //num[4] = Console.ReadLine();
+            //num[5] = Console.ReadLine();
+            //num[6] = Console.ReadLine();
+            //num[7] = Console.ReadLine();
+            //num[8] = Console.ReadLine();
+            //num[9] = Console.ReadLine();
+            //Array.Sort(num);
+            //Console.Write(num[0]);
+            //Console.Write(num[1]);
+            //Console.Write(num[2]);
+            //Console.Write(num[3]);
+            //Console.Write(num[4]);
+            //Console.Write(num[5]);
+            //Console.Write(num[6]);
+            //Console.Write(num[7]);
+            //Console.Write(num[8]);
+            //Console.Write(num[9]);
+
+            // 6 solution
+
+            //int[] array = { 2, 5, 8 };
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+
+            //    sum += array[i];
+            //}
+            //Console.WriteLine(sum);
+
+
+        }
+    }
+}
---
+++ 26.6.2024 new/26.6.2024 new/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26._6._2024_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 solution
+            //string num1 = Console.ReadLine();
+            //string num2 = Console.ReadLine();
+
+            //string[] num3 = {num1, num2};
+            //Array.Sort(num3);
+            //Console.WriteLine(num3[0]);
+
+            // 2 solution
+
+            //Console.WriteLine("Enter a number. : ");
+            //var num = int.Parse(Console.ReadLine());
+            //if (num > 0)
+            //{
+            //    Console.WriteLine("The Sign is +");
+            //}
+            //else if (num < 0)
+            //{
+            //    Console.WriteLine("The Sign is -");
+            //}
+
+
+            // 3 solution
+            //string num1 = Console.ReadLine();
+            //string num2 = Console.ReadLine();
+            //string num3 = Console.ReadLine();
+
+            //string[] num4 = { num1, num2,num3 };
+            //Array.Reverse(num4);
+            //Console.WriteLine(num4[0] +" " + num4[1]+" " + num4[2] );
+
+            // 4 solution
+            //string num1 = Console.ReadLine();
+            //string num2 = Console.ReadLine();
+            //string num3 = Console.ReadLine();
+            //string num4 = Console.ReadLine();
+            //string num5 = Console.ReadLine();
+
+            //string[] numall = { num1, num2, num3, num4, num5 };
+            //Array.Sort(numall);
+            //Console.WriteLine(numall[4]);
+
+            // 5 solution
+
+            //int kilometer= Convert.ToInt32(Console.ReadLine());
+            //double mile = kilometer * 0.6213;
+            //Console.WriteLine(mile);
+
+            // 6 solution
+
+            //int hours = Convert.ToInt32(Console.ReadLine());
+            //int minutes = Convert.ToInt32(Console.ReadLine());
+
+            //int hourstominutes= hours*60;
+            //int sumofmin= hourstominutes+ minutes;
+            //Console.WriteLine(sumofmin);
+
+            // 7 solutoin
+
+            //int minutes = Convert.ToInt32(Console.ReadLine());
+            ////int houre = minutes / 60;
+            //int min = minutes % 60;
+            //Console.WriteLine(houre + "," + min);
+
+            // 8 solution
+
+            //string[] arry = { "hello world ", "always say alhamdo lelah" };
+            //Console.WriteLine(arry[0] + arry[1]);
+            //Console.WriteLine(arry[0].Length);
+            //Console.WriteLine(arry[1].Length);
+
+
+        }
+    }
+}
